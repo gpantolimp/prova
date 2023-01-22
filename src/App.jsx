363e26152aa144687ccccd394787a9bbbcfaad9e
LarispPigasse <renato.casalena@gmail.com>
2023-01-22 18:13:24 +0100

    Add menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/sections/header"
 import Footer from "./components/sections/footer"
 import Content from "./components/sections/content"
+import Menu from "./components/sections/menu"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
       <Content />
       <Footer />
     </div>
