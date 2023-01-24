fa8a6a7850dc65b0395e3bfe23e7fb9864e94faf
LarispPigasse <renato.casalena@gmail.com>
2023-01-24 15:59:56 +0100

    Home temporanea con logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
+import MyLogo from "./assets/img/logo-light.png"
 function App() {
   return (
     <div className="App">
-CIAO
+      <div class="container mx-auto">
+        <div class="grid h-screen place-items-center">
+          <img src={MyLogo} alt="Express Delivery Group" />
+        </div>
+      </div>
     </div>
   )
 }
