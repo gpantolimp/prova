106f43d65e0020436d2f4848c781a0f5ce2f0a8e
LarispPigasse <renato.casalena@gmail.com>
2023-01-24 14:44:06 +0100

    progetto pulito

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import Header from "./components/sections/header"
-import Footer from "./components/sections/footer"
-import Content from "./components/sections/content"
-import Menu from "./components/sections/menu"
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Menu />
-      <Content />
-      <Footer />
+CIAO
     </div>
   )
 }
--- src/components/sections/menu.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Menu() {
-  return (
-    <div>Sono il Menu</div>
-  )
-}
-
-export default Menu
