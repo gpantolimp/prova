b411642459fcfcf9038fe5acdb0d42576dc1cbd3
LarispPigasse <renato.casalena@gmail.com>
2023-02-07 13:34:07 +0100

    aggiornamento navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1600px] mx-auto px-4 text-black'>
-      <h1 className='w-full text-3xl font-bold text-[#EE0000]'>TRACKING</h1>
+      <h1 className='w-full text-4xl font-bold text-[#EE0000]'>TRACKING</h1>
       <ul className='hidden md:flex'>
         <li className='p-4 font-semibold'>Dashboard</li>
         <li className='p-4 font-semibold'>Tracking</li>
@@ -21,7 +21,7 @@ function Navbar() {
         {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
       </div>
       <div className={!nav ? 'fixed left-0 top-0 w-[64%] h-full border-r border-r-gray-900 bg-stone-800 ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#EE0000] m-4 mt-8'>TRACKING</h1>
+        <h1 className='w-full text-4xl font-bold text-[#EE0000] m-4 mt-8'>TRACKING</h1>
         <ul className='pt-8 uppercase font-semibold text-white'>
           <li className='p-4 ml-2 border-b border-gray-300'>Dashboard</li>
           <li className='p-4 ml-2 border-b border-gray-300'>Tracking</li>
