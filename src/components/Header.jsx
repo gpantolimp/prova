04202192f2023c472f9685a82b9338445ca1a9cf
Gabriel Pantoli <utente-5@mediaprint.local>
2023-05-23 14:08:06 +0200

    New features

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@ import Navbar from "./Navbar"
 
 function Header() {
   return (
-    <div className=' bg-stone-700 border-b border-white'>
+    <div className='border-b border-white'>
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
+        <Navbar />
         <Navbar />
     </div>
   )
